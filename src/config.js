4d124acfebc2e84c8c710b756b008ac4779eac78
Scott Schroeder <snschroeder89@gmail.com>
2019-11-22 13:32:35 -0600

    update to origin

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN : process.env.CLIENT_ORIGIN || 'https://algorithme.now.sh',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin:asdf@localhost/spaced-rep',
